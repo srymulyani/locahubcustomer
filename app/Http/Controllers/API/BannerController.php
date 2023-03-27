6ca466602350e4b0cac1ecc999a719fe8cca611c
srymulyani <74601464+srymulyani@users.noreply.github.com>
2023-03-27 10:01:14 +0700

    Banner

--- app/Http/Controllers/API/BannerController.php
+++ app/Http/Controllers/API/BannerController.php
@@ -12,7 +12,7 @@ class BannerController extends Controller
     public function show($id)
     {
           
-        $image = Banner::findOrFail($id);
+        $image = Banner::findOrFail($id)->get();
 
         return response()->json([
             'url' => $image->url,
@@ -21,11 +21,17 @@ class BannerController extends Controller
     
 
     public function upload(Request $request){
-   
+    
+     $images = [];
      if ($request->hasFile('image1')) {
         $image = new Banner();
         $file = $request->file('image1');
 
+         // CHECK UKURAN GAMBAR
+        $max_file_size = 5000000; // ukuran maksimal file dalam byte (5MB)
+        if ($file->getSize() > $max_file_size) {
+            return response()->json(['error' => 'Ukuran file terlalu besar. Maksimal ' . ($max_file_size/1000000) . ' MB']);
+        }
         //CHECK EKSTENSI GAMBAR
         $allowed_exts = ['jpg', 'jpeg', 'png', 'gif', 'svg'];
         $ext = $file->getClientOriginalExtension();
@@ -39,39 +45,7 @@ class BannerController extends Controller
         $images[] = $image;
     }
 
-    if ($request->hasFile('image2')) {
-        $image = new Banner();
-         $file = $request->file('image2');
-
-        //CHECK EKSTENSI GAMBAR
-        $allowed_exts = ['jpg', 'jpeg', 'png', 'gif', 'svg'];
-        $ext = $file->getClientOriginalExtension();
-        if (!in_array($ext, $allowed_exts)) {
-            return response()->json(['error' => 'File harus berupa gambar dengan format jpg, jpeg, png, atau svg']);
-        }
-
-        $path = $request->file('image2')->storeAs('Banner', $request->file('image2')->getClientOriginalName(),'public');
-        $image->url = 'storage/' . $path;
-        $image->save();
-        $images[] = $image;
-    }
-
-     if ($request->hasFile('image3')) {
-        $image = new Banner();
-        $file = $request->file('image3');
-
-        //CHECK EKSTENSI GAMBAR
-        $allowed_exts = ['jpg', 'jpeg', 'png', 'gif', 'svg'];
-        $ext = $file->getClientOriginalExtension();
-        if (!in_array($ext, $allowed_exts)) {
-            return response()->json(['error' => 'File harus berupa gambar dengan format jpg, jpeg, png, atau svg']);
-        }
-        $path = $request->file('image3')->storeAs('Banner', $request->file('image3')->getClientOriginalName(),'public');
-        $image->url = 'storage/' . $path;
-        $image->save();
-        $images[] = $image;
-    }
-
     return response()->json(['result' => $images]);
     }
+
 }
